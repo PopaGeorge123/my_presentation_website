195009d3eec0a0420cfe430e3c7aad68c200384c
Popa George <popageo02@gmail.com>
2023-08-10 10:51:15 +0300

    added all requared

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Node app worked!")
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from my Node.js app hosted on Netlify!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
